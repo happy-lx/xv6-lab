42e2e3dfb8f790ecaad3b5b74520b294ad950426
lixin <1037997956@qq.com>
2022-09-13 10:12:21 +0800

    complete thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t mutex[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -40,6 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(mutex + i);
 
   // is the key already present?
   struct entry *e = 0;
@@ -54,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(mutex + i);
 
 }
 
@@ -114,6 +117,9 @@ main(int argc, char *argv[])
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
   assert(NKEYS % nthread == 0);
+  for(int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(mutex + i, NULL);
+  }
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
